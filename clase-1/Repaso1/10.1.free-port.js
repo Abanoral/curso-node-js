b05500a26f0cafda65dbd2b421579fdf365a478d
Alejandro <66677531+Abanoral@users.noreply.github.com>
2024-02-06 22:24:16 +0100

    repaso 9.http y 10.free

---
+++ clase-1/Repaso1/10.1.free-port.js
@@ -0,0 +1,26 @@
+// es un tipo de conexion tcp, es mas rapido porque no tiene que enviar tantas cabeceras
+// nos permitira tambien averiguar si ese servidor esta abierto o no, y levantarlo si es necesario
+const net = require('node:net')
+
+function findAvailablePort (desiredPort) {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer()
+
+    server.listen(desiredPort, () => {
+      const { port } = server.address()
+      server.close(() => {
+        resolve(port)
+      })
+    })
+
+    server.on('error', (err) => {
+      if (err.code === 'EADDRINUSE') {
+        findAvailablePort(0).then(port => resolve(port))
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+module.exports = { findAvailablePort }
